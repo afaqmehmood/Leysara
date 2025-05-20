3293bce035b8550677d393fb6fde7d48b68fc548
Afaq Mehmood <afaq171@gmail.com>
2025-05-20 16:50:30 +0500

    first commit

--- assets/quick-add.js
+++ assets/quick-add.js
@@ -1,246 +1,122 @@
 if (!customElements.get('quick-add-modal')) {
-	customElements.define(
-		'quick-add-modal',
-		class QuickAddModal extends ModalDialog {
-			constructor() {
-				super()
-				this.modalContent = this.querySelector('[id^="QuickAddInfo-"]')
+  customElements.define(
+    'quick-add-modal',
+    class QuickAddModal extends ModalDialog {
+      constructor() {
+        super();
+        this.modalContent = this.querySelector('[id^="QuickAddInfo-"]');
 
-				this.addEventListener('keyup', (event) => {
-					if (event.code.toUpperCase() === 'ESCAPE') this.hide()
-				})
+        this.addEventListener('product-info:loaded', ({ target }) => {
+          target.addPreProcessCallback(this.preprocessHTML.bind(this));
+        });
+      }
 
-				this.querySelector('.quick-add-modal__overlay')?.addEventListener(
-					'click',
-					this.hide.bind(this, false)
-				)
+      hide(preventFocus = false) {
+        const cartNotification = document.querySelector('cart-notification') || document.querySelector('cart-drawer');
+        if (cartNotification) cartNotification.setActiveElement(this.openedBy);
+        this.modalContent.innerHTML = '';
 
-				//window.addEventListener('resize', function () {
-				//	$('.js-media-list').each(function () {
-				//		this.swiper?.destroy();
-				//	});
-				//	$('.js-media-sublist').each(function () {
-				//		this.swiper?.destroy();
-				//	});
+        if (preventFocus) this.openedBy = null;
+        super.hide();
+      }
 
-				//	setTimeout(() => {
-				//		subSliderInit(true);
-				//		sliderInit(true);
-				//	}, 200)
-				//});
-			}
+      show(opener) {
+        opener.setAttribute('aria-disabled', true);
+        opener.classList.add('loading');
+        opener.querySelector('.loading__spinner').classList.remove('hidden');
 
-			hide(preventFocus = false) {
-				const cartDrawer = document.querySelector('cart-drawer')
-				if (cartDrawer) cartDrawer.setActiveElement(this.openedBy)
-				this.modalContent.innerHTML = ''
+        fetch(opener.getAttribute('data-product-url'))
+          .then((response) => response.text())
+          .then((responseText) => {
+            const responseHTML = new DOMParser().parseFromString(responseText, 'text/html');
+            const productElement = responseHTML.querySelector('product-info');
 
-				$('.js-media-list').each(function () {
-					this.swiper?.destroy()
-				})
-				$('.js-media-sublist').each(function () {
-					this.swiper?.destroy()
-				})
+            this.preprocessHTML(productElement);
+            HTMLUpdateUtility.setInnerHTML(this.modalContent, productElement.outerHTML);
 
-				subSliderInit(true)
-				sliderInit(true)
+            if (window.Shopify && Shopify.PaymentButton) {
+              Shopify.PaymentButton.init();
+            }
+            if (window.ProductModel) window.ProductModel.loadShopifyXR();
 
-				if (preventFocus) this.openedBy = null
-				super.hide()
+            super.show(opener);
+          })
+          .finally(() => {
+            opener.removeAttribute('aria-disabled');
+            opener.classList.remove('loading');
+            opener.querySelector('.loading__spinner').classList.add('hidden');
+          });
+      }
 
-				document.body.classList.remove('overflow-hidden-modal')
-				//location.reload()
-			}
+      preprocessHTML(productElement) {
+        productElement.classList.forEach((classApplied) => {
+          if (classApplied.startsWith('color-') || classApplied === 'gradient')
+            this.modalContent.classList.add(classApplied);
+        });
+        this.preventDuplicatedIDs(productElement);
+        this.removeDOMElements(productElement);
+        this.removeGalleryListSemantic(productElement);
+        this.updateImageSizes(productElement);
+        this.preventVariantURLSwitching(productElement);
+      }
 
-			show(opener) {
-				opener.setAttribute('aria-disabled', true)
-				opener.classList.add('loading')
+      preventVariantURLSwitching(productElement) {
+        productElement.setAttribute('data-update-url', 'false');
+      }
 
-				if (opener.querySelector('.loading-overlay__spinner')) {
-					opener
-						.querySelector('.loading-overlay__spinner')
-						.classList.remove('hidden')
-				}
+      removeDOMElements(productElement) {
+        const pickupAvailability = productElement.querySelector('pickup-availability');
+        if (pickupAvailability) pickupAvailability.remove();
 
-				fetch(opener.getAttribute('data-product-url'))
-					.then((response) => response.text())
-					.then((responseText) => {
-						const responseHTML = new DOMParser().parseFromString(
-							responseText,
-							'text/html'
-						)
-						this.productElement = responseHTML.querySelector(
-							'section[id^="MainProduct-"]'
-						)
-						this.preventDuplicatedIDs()
-						this.removeDOMElements()
-						this.setInnerHTML(
-							this.modalContent,
-							this.productElement.innerHTML,
-							opener
-						)
+        const productModal = productElement.querySelector('product-modal');
+        if (productModal) productModal.remove();
 
-						if (window.Shopify && Shopify.PaymentButton) {
-							Shopify.PaymentButton.init()
-						}
+        const modalDialog = productElement.querySelectorAll('modal-dialog');
+        if (modalDialog) modalDialog.forEach((modal) => modal.remove());
+      }
 
-						if (window.ProductModel) window.ProductModel.loadShopifyXR()
+      preventDuplicatedIDs(productElement) {
+        const sectionId = productElement.dataset.section;
 
-						this.removeGalleryListSemantic()
-						this.updateImageSizes()
-						this.preventVariantURLSwitching()
-						super.show(opener)
+        const oldId = sectionId;
+        const newId = `quickadd-${sectionId}`;
+        productElement.innerHTML = productElement.innerHTML.replaceAll(oldId, newId);
+        Array.from(productElement.attributes).forEach((attribute) => {
+          if (attribute.value.includes(oldId)) {
+            productElement.setAttribute(attribute.name, attribute.value.replace(oldId, newId));
+          }
+        });
 
-						document.body.classList.add('overflow-hidden-modal')
-					})
-					.finally(() => {
-						opener.removeAttribute('aria-disabled')
-						opener.classList.remove('loading')
+        productElement.dataset.originalSection = sectionId;
+      }
 
-						if (opener.querySelector('.loading-overlay__spinner')) {
-							opener
-								.querySelector('.loading-overlay__spinner')
-								.classList.add('hidden')
-						}
+      removeGalleryListSemantic(productElement) {
+        const galleryList = productElement.querySelector('[id^="Slider-Gallery"]');
+        if (!galleryList) return;
 
-						subSliderInit(true)
-						sliderInit(true)
-					})
-			}
+        galleryList.setAttribute('role', 'presentation');
+        galleryList.querySelectorAll('[id^="Slide-"]').forEach((li) => li.setAttribute('role', 'presentation'));
+      }
 
-			setInnerHTML(element, html, opener) {
-				element.innerHTML = html
+      updateImageSizes(productElement) {
+        const product = productElement.querySelector('.product');
+        const desktopColumns = product?.classList.contains('product--columns');
+        if (!desktopColumns) return;
 
-				// Reinjects the script tags to allow execution. By default, scripts are disabled when using element.innerHTML.
-				element.querySelectorAll('script').forEach((oldScriptTag) => {
-					const newScriptTag = document.createElement('script')
-					Array.from(oldScriptTag.attributes).forEach((attribute) => {
-						newScriptTag.setAttribute(attribute.name, attribute.value)
-					})
-					newScriptTag.appendChild(
-						document.createTextNode(oldScriptTag.innerHTML)
-					)
-					oldScriptTag.parentNode.replaceChild(newScriptTag, oldScriptTag)
-				})
+        const mediaImages = product.querySelectorAll('.product__media img');
+        if (!mediaImages.length) return;
 
-				// Read more button
-				const moreBtn = document.createElement('a')
-				moreBtn.innerHTML = `<span>${theme.quickviewMore}</span>
-														<svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
-															<path d="M3.25 9.25L6.75 5.75L3.25 2.25" stroke="currentColor" stroke-width="1.3" stroke-linecap="round" stroke-linejoin="round" />
-														</svg>`
-				moreBtn.setAttribute('href', opener.getAttribute('data-product-url'))
-				moreBtn.setAttribute('class', 'product__full-details button')
-				if (
-					element.querySelectorAll('.product__info-column') &&
-					element.querySelectorAll('.product__info-column').length > 0
-				) {
-					element.querySelectorAll('.product__info-column').forEach((el, i) => {
-						if (i === 1) {
-							el.appendChild(moreBtn)
-						}
-					})
-				} else {
-					element.querySelector('.product__info-container').appendChild(moreBtn)
-				}
-			}
+        let mediaImageSizes =
+          '(min-width: 1000px) 715px, (min-width: 750px) calc((100vw - 11.5rem) / 2), calc(100vw - 4rem)';
 
-			removeDOMElements() {
-				const modal = this.productElement.querySelector('product-modal')
-				if (modal) modal.remove()
+        if (product.classList.contains('product--medium')) {
+          mediaImageSizes = mediaImageSizes.replace('715px', '605px');
+        } else if (product.classList.contains('product--small')) {
+          mediaImageSizes = mediaImageSizes.replace('715px', '495px');
+        }
 
-				const breadcrumb = this.productElement.querySelectorAll('.breadcrumb')
-				breadcrumb.forEach((el) => {
-					const container = el.closest('.container')
-					if (container) {
-						container.remove()
-					}
-				})
-
-				const popup = this.productElement.querySelectorAll('.product-popup')
-				popup.forEach((el) => {
-					el.remove()
-				})
-
-				const sku = this.productElement.querySelector('.product__sku')
-				if (sku) sku.remove()
-
-				const about = this.productElement.querySelectorAll('.about')
-				about.forEach((el) => {
-					el.remove()
-				})
-
-				const customLiquid =
-					this.productElement.querySelectorAll('.custom-liquid')
-				customLiquid.forEach((el) => {
-					el.remove()
-				})
-
-				const shareButtons = this.productElement.querySelector('.share-buttons')
-				if (shareButtons) shareButtons.remove()
-
-				const tags = this.productElement.querySelector('.product-tags')
-				if (tags) tags.remove()
-
-				const pickupAvailability = this.productElement.querySelector(
-					'.pickup-availability'
-				)
-				if (pickupAvailability) pickupAvailability.remove()
-			}
-
-			preventDuplicatedIDs() {
-				const sectionId = this.productElement.dataset.section
-				this.productElement.innerHTML =
-					this.productElement.innerHTML.replaceAll(
-						sectionId,
-						`quickadd-${sectionId}`
-					)
-				this.productElement
-					.querySelectorAll('variant-selects, variant-radios')
-					.forEach((variantSelect) => {
-						variantSelect.dataset.originalSection = sectionId
-					})
-			}
-
-			preventVariantURLSwitching() {
-				if (this.modalContent.querySelector('variant-radios,variant-selects')) {
-					this.modalContent
-						.querySelector('variant-radios,variant-selects')
-						.setAttribute('data-update-url', 'false')
-				}
-			}
-
-			removeGalleryListSemantic() {
-				const galleryList = this.modalContent.querySelector(
-					'[id^="Slider-Gallery"]'
-				)
-				if (!galleryList) return
-
-				galleryList.setAttribute('role', 'presentation')
-				galleryList
-					.querySelectorAll('[id^="Slide-"]')
-					.forEach((li) => li.setAttribute('role', 'presentation'))
-			}
-
-			updateImageSizes() {
-				const product = this.modalContent.querySelector('.product')
-				const desktopColumns = product.classList.contains('product--columns')
-				if (!desktopColumns) return
-
-				const mediaImages = product.querySelectorAll('.product__media img')
-				if (!mediaImages.length) return
-
-				let mediaImageSizes =
-					'(min-width: 1000px) 715px, (min-width: 750px) calc((100vw - 11.5rem) / 2), calc(100vw - 4rem)'
-
-				if (product.classList.contains('product--medium')) {
-					mediaImageSizes = mediaImageSizes.replace('715px', '605px')
-				} else if (product.classList.contains('product--small')) {
-					mediaImageSizes = mediaImageSizes.replace('715px', '495px')
-				}
-
-				mediaImages.forEach((img) => img.setAttribute('sizes', mediaImageSizes))
-			}
-		}
-	)
+        mediaImages.forEach((img) => img.setAttribute('sizes', mediaImageSizes));
+      }
+    }
+  );
 }
